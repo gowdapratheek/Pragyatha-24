78b7d5a105020b7cef79c073f37619715da1e477
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-09-27 18:32:03 +0530

    initial Commit
    Welcome to Pragyatha 24

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+import { Button } from "@/components/ui/button"
+function App() {
+
+
+  return (
+    <>
+      <div className="text-4xl font-bold">Pragyatha-24</div>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
